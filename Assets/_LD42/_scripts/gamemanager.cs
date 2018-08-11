9728b4ecc259db2e74c809b326b026e6e1736504
Will Wheatley <bill@williamwheatley.com>
2018-08-11 07:29:47 -0400

    Settled on a pixels per unit i liked

--- Assets/_LD42/_scripts/gamemanager.cs
+++ Assets/_LD42/_scripts/gamemanager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class gamemanager : MonoBehaviour {
 
@@ -9,6 +10,12 @@ public class gamemanager : MonoBehaviour {
     public int[,] map;
     public Sprite baseHex;
     public static gamemanager instance;
+
+    public Tilemap tileMap;
+    public Tile selectedTile;
+
+    public int seed = 12345;
+    
 
     private void Awake()
     {
@@ -28,7 +35,20 @@ public class gamemanager : MonoBehaviour {
     // Use this for initialization
     void Start () {
 		
+        //Init our map
         GenerateMap(mapWidth, mapHeight);
+
+        //Perlin Noise it up
+        //PerlinNoise(map, seed);
+
+        //Randomly set 0,0 and 10,10 just to see where they are to the map
+        map[0, 0] = 1;
+        map[9, 9] = 1;
+            
+
+        //Render Map
+        RenderMap(map, tileMap, selectedTile);
+
 
 
 	}
@@ -39,7 +59,7 @@ public class gamemanager : MonoBehaviour {
 	}
 
 
-    public void GenerateMap(int width, int height, bool empty = false) {
+    public void GenerateMap(int width, int height, bool empty = true) {
 
         map = new int[width, height];
 
@@ -59,5 +79,61 @@ public class gamemanager : MonoBehaviour {
 
     }
 
+    //Render Full tiles
+    public void RenderMap(int[,] map, Tilemap tilemap, TileBase tile) {
+        Debug.Log(string.Format("Render Map - W{0}H{1}", map.GetUpperBound(0), map.GetUpperBound(1) ));
+
+
+        //Clear in case we have leftovers, regenerations etc
+        tilemap.ClearAllTiles();
+
+        //Loop through map
+        for (int x = 0; x <= map.GetUpperBound(0); x++) {
+            for (int y = 0; y <= map.GetUpperBound(1); y++) {
+                // 1 = Tile 0 = Nada
+                if (map[x, y] == 1) {
+                    Debug.Log(string.Format("X {0} Y {1} Set as 1", x,y ));
+                    tilemap.SetTile(new Vector3Int(x,y,0), tile);
+                }
+            }
+        }
+
+    }
+
+    //Clear Empty Hex's
+    public void ReDrawMap(int[,] map, Tilemap tilemap, TileBase tile) {
+        //Loop through map
+        for (int x = 0; x <= map.GetUpperBound(0); x++)
+        {
+            for (int y = 0; y <= map.GetUpperBound(1); y++)
+            {
+                // 1 = Tile 0 = Nada
+                if (map[x, y] == 0)
+                {
+                    tilemap.SetTile(new Vector3Int(x, y, 0), null);
+                }
+            }
+        }
+    }
+
+
+    public void PerlinNoise(int[,] map, float seed)
+    {
+        int newPoint;
+        //Used to reduced the position of the Perlin point
+        float reduction = 0.5f;
+        //Create the Perlin
+        for (int x = 0; x < map.GetUpperBound(0); x++)
+        {
+            newPoint = Mathf.FloorToInt((Mathf.PerlinNoise(x, seed) - reduction) * map.GetUpperBound(1));
+
+            //Make sure the noise starts near the halfway point of the height
+            newPoint += (map.GetUpperBound(1) / 2);
+            for (int y = newPoint; y >= 0; y--)
+            {
+                map[x, y] = 1;
+            }
+        }
+    }
 
 }
